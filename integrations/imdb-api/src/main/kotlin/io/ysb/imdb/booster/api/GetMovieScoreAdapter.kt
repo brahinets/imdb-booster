2f6504390e053f91052b833347dc347fabc99b8d
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-14 11:11:54 +0200

    Remove almost useless abstraction

--- integrations/imdb-api/src/main/kotlin/io/ysb/imdb/booster/api/GetMovieScoreAdapter.kt
+++ integrations/imdb-api/src/main/kotlin/io/ysb/imdb/booster/api/GetMovieScoreAdapter.kt
@@ -4,13 +4,14 @@ import io.ysb.imdb.booster.api.model.Model
 import io.ysb.imdb.booster.domain.MovieId
 import io.ysb.imdb.booster.domain.port.GetMovieScorePort
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
 import java.util.*
 
 @Component
-class GetMovieScoreAdapter(val imdbClient: ImdbApiClient) : GetMovieScorePort {
+class GetMovieScoreAdapter(val imdbClient: WebClient) : GetMovieScorePort {
 
     override fun getMovieScore(movieId: MovieId): Optional<Int> {
-        val get: Model = imdbClient.get(movieId)
+        val get: Model = getMovieResponse(movieId)
 
         val titles = get.data.titles
         if (titles.isEmpty()) {
@@ -20,5 +21,20 @@ class GetMovieScoreAdapter(val imdbClient: ImdbApiClient) : GetMovieScorePort {
         val value = titles.first().userRating.value
 
         return Optional.of(value)
+    }
+
+    private fun getMovieResponse(movieId: MovieId): Model {
+        return imdbClient.get()
+            .uri { uriBuilder -> uriBuilder
+                .query("operationName={operationName}&variables={variables}&extensions={extensions}")
+                .build(mapOf(
+                    Pair("operationName",  "TitlesUserRatings"),
+                    Pair("variables",  "{\"idArray\":[\"$movieId\"]}"),
+                    Pair("extensions",  "{\"persistedQuery\":{\"sha256Hash\":\"7895b806b91031c960384b9f47f276f45ecf5e817b5126b03ae754151c7bd530\",\"version\":1}}")
+                ))
+            }
+            .retrieve()
+            .bodyToMono(Model::class.java)
+            .block()!!
     }
 }
