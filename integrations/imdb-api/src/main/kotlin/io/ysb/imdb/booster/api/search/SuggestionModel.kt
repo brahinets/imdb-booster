558e492bd7d8a2a12f77376382472c0768378ddb
Yaroslav Brahinets <y.brahinets@gmail.com>
2023-12-22 16:24:36 +0200

    Fix autosuggest. Result type fields are optional

--- integrations/imdb-api/src/main/kotlin/io/ysb/imdb/booster/api/search/SuggestionModel.kt
+++ integrations/imdb-api/src/main/kotlin/io/ysb/imdb/booster/api/search/SuggestionModel.kt
@@ -18,8 +18,8 @@ data class D(
     @JsonProperty("i") val i: I?,
     @JsonProperty("id") val id: String,
     @JsonProperty("l") val title: String?,
-    @JsonProperty("q") val q: String,
-    @JsonProperty("qid") val qid: String,
+    @JsonProperty("q") val q: String?,
+    @JsonProperty("qid") val qid: String?,
     @JsonProperty("rank") val rank: Int?,
     @JsonProperty("s") val s: String,
     @JsonProperty("y") val year: Int
