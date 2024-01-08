d711e9188bc5c02dca08200347b85d3e2381bb3a
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-08 18:26:42 +0200

    Original name is required

--- domain/src/main/kotlin/io/ysb/imdb/booster/port/output/KpLocalTitle.kt
+++ domain/src/main/kotlin/io/ysb/imdb/booster/port/output/KpLocalTitle.kt
@@ -2,7 +2,7 @@ package io.ysb.imdb.booster.port.output
 
 data class KpLocalTitle(
     val name: String,
-    val originalName: String?,
+    val originalName: String,
     val myRating: Int,
     val year: Int,
     val genres: Set<String>
