2f4d2dec8246d2e842e6dd76c7f72ab7493aef92
Yaroslav Brahinets <y.brahinets@gmail.com>
2024-01-12 21:10:08 +0200

    Specify year criteria for better results

--- integrations/imdb-api/src/main/kotlin/io/ysb/imdb/booster/api/search/TitleSearchAdapter.kt
+++ integrations/imdb-api/src/main/kotlin/io/ysb/imdb/booster/api/search/TitleSearchAdapter.kt
@@ -1,5 +1,6 @@
 package io.ysb.imdb.booster.api.search
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ysb.imdb.booster.domain.TitleId
 import io.ysb.imdb.booster.port.output.SearchTitlePort
 import io.ysb.imdb.booster.port.output.TitleSearchCriteria
@@ -12,30 +13,40 @@ import java.util.*
 @Component
 class TitleSearchAdapter(val imdbClient: WebClient) : SearchTitlePort {
 
+    private val logger = KotlinLogging.logger {}
+
     override fun searchTitle(criteria: TitleSearchCriteria): Optional<TitleSuggestion> {
-        val suggestion: Optional<TitleSuggestion> =
-            if (criteria.originalName.lowercase() === criteria.localisedName.lowercase()) {
-                searchTitleByLocalisedName(criteria.localisedName)
+        val localTitle = criteria.originalName.lowercase() == criteria.localisedName.lowercase()
+        val suggestion: List<TitleSuggestion> =
+            if (localTitle) {
+                searchTitlesByLocalisedName(criteria)
             } else {
-                searchTitleByName(criteria.originalName)
+                searchTitlesByName(criteria)
             }
 
-        return suggestion.filter { it.year == criteria.year }
+        return if (suggestion.isEmpty()) {
+            Optional.empty()
+        } else if (suggestion.size > 1) {
+            logger.warn { ("More than one title found for criteria $criteria") }
+            Optional.empty()
+        } else {
+            Optional.of(suggestion.first())
+        }
     }
 
-    private fun searchTitleByName(titleName: String): Optional<TitleSuggestion> {
-        val request = buildRequest(titleName)
+    private fun searchTitlesByName(criteria: TitleSearchCriteria): List<TitleSuggestion> {
+        val request = buildRequest(criteria.originalName, criteria.year)
 
         val response = doRequest(request)
-        return Optional.ofNullable(mapResults(response.d, titleName).firstOrNull())
+        return mapResults(response.d, criteria.originalName)
     }
 
-    private fun searchTitleByLocalisedName(titleName: String): Optional<TitleSuggestion> {
-        val request = buildRequest(titleName)
+    private fun searchTitlesByLocalisedName(criteria: TitleSearchCriteria): List<TitleSuggestion> {
+        val request = buildRequest(criteria.localisedName, criteria.year)
             .header("X-Imdb-User-Country", "RU")
 
         val response = doRequest(request)
-        return Optional.ofNullable(mapResults(response.d, titleName).firstOrNull())
+        return mapResults(response.d, criteria.localisedName)
     }
 
     private fun doRequest(header: WebClient.RequestHeadersSpec<*>) =
@@ -44,10 +55,10 @@ class TitleSearchAdapter(val imdbClient: WebClient) : SearchTitlePort {
             .bodyToMono(SuggestionModel::class.java)
             .block()!!
 
-    private fun buildRequest(titleId: TitleId): WebClient.RequestHeadersSpec<*> {
+    private fun buildRequest(titleId: TitleId, year: Int): WebClient.RequestHeadersSpec<*> {
         val builder = UriComponentsBuilder
-            .fromHttpUrl("https://v3.sg.media-imdb.com/suggestion/x/{titleId}.json")
-            .buildAndExpand(titleId)
+            .fromHttpUrl("https://v3.sg.media-imdb.com/suggestion/x/{titleId} {year}.json")
+            .buildAndExpand(titleId, year)
 
         return imdbClient.get()
             .uri(builder.toUri())
